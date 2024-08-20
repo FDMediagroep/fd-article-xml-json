00ca551581df3a2f63dd503ce00935f4c7208a79
Willem Liu <willem.liu@fdmediagroep.nl>
2024-08-20 14:37:13 +0200

    Added summary alignment

--- src/elements/summary.ts
+++ src/elements/summary.ts
@@ -3,11 +3,13 @@ import { fdmgObject, findElement } from '../utils';
 export interface fdmgSummary extends fdmgObject {
     title?: string;
     summaries?: string[];
+    alignment?: string;
 }
 export const getSummary = (element: fdmgObject): fdmgSummary => {
     return {
         name: element.name,
         title: element.attributes?.title,
         summaries: findElement(element.children, 'fdmg-content').split('\n'),
+        alignment: findElement(element.children, 'fdmg-alignment'),
     };
 };
